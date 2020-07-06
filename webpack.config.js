22ab8709d6b0b478d92b7c8a90c80047ac10e20b
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-07-06 18:36:19 -0400

    initial build
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+
+const path = require("path")
+
+const minifiedConfig = {}
+
+module.exports = {
+    entry: "./build/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "dateplus.bundle.js",
+        library: "dateplus",
+        libraryTarget: "commonjs2",
+    },
+    mode: "production",
+}
