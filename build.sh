2d267876b2cdc818cb75ddd039a6b0fa36be03e6
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-07-07 23:53:42 -0400

    add logs and concurrent compiling to build script
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- build.sh
+++ build.sh
@@ -3,39 +3,56 @@
 # Copyright (C) 2020 Luke Zhang | MIT License | luke-zhang-04.github.io
 
 # Compile sass for docs (not really necessary)
-sass docs/scss/style.scss docs/styles.css --style compressed
+printf "Compiling docs/scss/style.scss with SASS\n"
+sass docs/scss/style.scss docs/styles.css --style compressed &
 
 # Compile typescript
-npx tsc -p .
+printf "Compiling TypeScript\n"
+npx tsc -p . &
+
+wait
 
 sed -i -e 's/src/lib/g' index.js
 rm -rf index.js-e
 
+printf "Moving files to ./build\n"
 mkdir build
 cd src
 find . -name "*.js" -exec mv "{}" ../build \;
 cd ..
 
 # Convert to commonJs with Babel
-npx babel index.js -o index.js
+printf "Compiling index.js with Babel\n"
+npx babel index.js -o index.js &
 
 # Run Webpack on ./build
-npx webpack
+printf "Bundling ./build/index.js with Webpack\n"
+npx webpack &
+
+wait
 
 # Copy bundle
+printf "Copying dist bundle\n"
 cp ./dist/deStagnate.bundle.js ./dist/deStagnate.bundle.min.js
 
 # Minify copy of bundle
-npx babel ./dist/deStagnate.bundle.min.js -o ./dist/deStagnate.bundle.min.js --minified --compact true --no-comments -s inline
+printf "Compiling and minifying distribution files with Babel\n"
+npx babel ./dist/deStagnate.bundle.min.js -o ./dist/deStagnate.bundle.min.js --minified --compact true --no-comments -s inline &
 
 # Run babel on bundle
-npx babel ./dist/deStagnate.bundle.js -o ./dist/deStagnate.bundle.js -s inline
+printf "Compiling distribution files with Babel\n"
+npx babel ./dist/deStagnate.bundle.js -o ./dist/deStagnate.bundle.js -s inline &
+
+wait
 
 # Remove previous lib
+printf "Clearning up...\n"
 if [ -d "lib" ]; then
     rm -r lib
 fi
 
+printf "Compiling build to lib with Babel\n"
 npx babel build --out-dir lib
 
+printf "Clearning up...\n"
 rm -r build
