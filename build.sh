e269f5a7b30126b4baf5e9bb31f689983dc90d25
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-07-08 09:43:17 -0400

    add ability to watch for files
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- build.sh
+++ build.sh
@@ -2,57 +2,80 @@
 # Build shell script for DeStagnate
 # Copyright (C) 2020 Luke Zhang | MIT License | luke-zhang-04.github.io
 
-# Compile sass for docs (not really necessary)
-printf "Compiling docs/scss/style.scss with SASS\n"
-sass docs/scss/style.scss docs/styles.css --style compressed &
+build() {
+    # Compile sass for docs (not really necessary)
+    printf "Compiling docs/scss/style.scss with SASS\n"
+    sass docs/scss/style.scss docs/styles.css --style compressed &
 
-# Compile typescript
-printf "Compiling TypeScript\n"
-npx tsc -p . &
+    # Compile typescript
+    printf "Compiling TypeScript\n"
+    npx tsc -p . &
 
-wait
+    wait
 
-sed -i -e 's/src/lib/g' index.js
-rm -rf index.js-e
+    sed -i -e 's/src/lib/g' index.js
+    rm -rf index.js-e
 
-printf "Moving files to ./build\n"
-mkdir build
-cd src
-find . -name "*.js" -exec mv "{}" ../build \;
-cd ..
+    printf "Moving files to ./build\n"
+    mkdir build
+    cd src
+    find . -name "*.js" -exec mv "{}" ../build \;
+    cd ..
 
-# Convert to commonJs with Babel
-printf "Compiling index.js with Babel\n"
-npx babel index.js -o index.js &
+    # Convert to commonJs with Babel
+    printf "Compiling index.js with Babel\n"
+    npx babel index.js -o index.js &
 
-# Run Webpack on ./build
-printf "Bundling ./build/index.js with Webpack\n"
-npx webpack &
+    # Run Webpack on ./build
+    printf "Bundling ./build/index.js with Webpack\n"
+    npx webpack &
 
-wait
+    wait
 
-# Copy bundle
-printf "Copying dist bundle\n"
-cp ./dist/deStagnate.bundle.js ./dist/deStagnate.bundle.min.js
+    # Copy bundle
+    printf "Copying dist bundle\n"
+    cp ./dist/deStagnate.bundle.js ./dist/deStagnate.bundle.min.js
 
-# Minify copy of bundle
-printf "Compiling and minifying distribution files with Babel\n"
-npx babel ./dist/deStagnate.bundle.min.js -o ./dist/deStagnate.bundle.min.js --minified --compact true --no-comments -s inline &
+    # Minify copy of bundle
+    printf "Compiling and minifying distribution files with Babel\n"
+    npx babel ./dist/deStagnate.bundle.min.js -o ./dist/deStagnate.bundle.min.js --minified --compact true --no-comments -s inline &
 
-# Run babel on bundle
-printf "Compiling distribution files with Babel\n"
-npx babel ./dist/deStagnate.bundle.js -o ./dist/deStagnate.bundle.js -s inline &
+    # Run babel on bundle
+    printf "Compiling distribution files with Babel\n"
+    npx babel ./dist/deStagnate.bundle.js -o ./dist/deStagnate.bundle.js -s inline &
 
-wait
+    wait
 
-# Remove previous lib
-printf "Clearning up...\n"
-if [ -d "lib" ]; then
-    rm -r lib
+    # Remove previous lib
+    printf "Clearning up...\n"
+    if [ -d "lib" ]; then
+        rm -r lib
+    fi
+
+    printf "Compiling build to lib with Babel\n"
+    npx babel build --out-dir lib
+
+    printf "Clearning up...\n"
+    rm -r build
+}
+
+watch() {
+    fileChange1=""
+
+    while [[ true ]]; do
+        fileChange2=`find src/ -type f -exec md5 {} \;`
+        if [[ $fileChange1 != $fileChange2 ]] ; then           
+            build
+            fileChange1=$fileChange2
+            printf "Waiting...\n"
+        fi
+        sleep 10
+        printf "Sleep\n"
+    done
+}
+
+if [[ "$1"==="--watch" ]]; then
+    watch
+else
+    build
 fi
-
-printf "Compiling build to lib with Babel\n"
-npx babel build --out-dir lib
-
-printf "Clearning up...\n"
-rm -r build
