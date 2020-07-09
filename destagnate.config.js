cd60b80fdaf12765fb7437c794c673cc9a67f18f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-07-09 17:48:37 -0400

    update config file and ignore in NPM
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- destagnate.config.js
+++ destagnate.config.js
@@ -4,17 +4,11 @@ const calculator = {
         entry: "./docs/src/calculator.js",
         output: "docs/examples/calculator.js",
         mode: "development",
-        typescript: { // If typescript found only
-            esModuleInterop: true,
-        }
     },
     counter= {
         entry: "./docs/src/counter.js",
         output: "docs/examples/counter.js",
         mode: "development",
-        typescript: { // If typescript found only
-            esModuleInterop: true,
-        }
     }
 
 module.exports = [calculator, counter]
