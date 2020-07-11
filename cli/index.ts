79d4ebf216b06c2eef988fd71a1b47cf07c7bb2a
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-07-11 12:04:43 -0400

    fix(setState): allow Typescript for setting parial state, throw error if new state key given
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- cli/index.ts
+++ cli/index.ts
@@ -5,7 +5,7 @@
  * @copyright Copyright (C) 2020 Luke Zhang
  * @author Luke Zhang luke-zhang-04.github.io
  * @license MIT
- * @version 1.2.1
+ * @version 1.2.2
  */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, global-require */
 import * as Babel from "@babel/core"
