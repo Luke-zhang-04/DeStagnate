1b9fc3228d31c62686fca7eaf5a351a3b2ff815f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-07-27 12:03:26 -0400

    add ref example to config
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- destagnate.config.js
+++ destagnate.config.js
@@ -14,6 +14,11 @@ const calculator = {
         entry: "./docs/src/tictactoe.ts",
         output: "docs/compiled/tictactoe.js",
         mode: "development"
+    },
+    ref = {
+        entry: "./docs/src/ref.ts",
+        output: "docs/compiled/ref.js",
+        mode: "development"
     }
 
-module.exports = [calculator, counter, nest]
+module.exports = [calculator, counter, nest, ref]
