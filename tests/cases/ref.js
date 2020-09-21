1a3153d3621dfb3e4c0ca11987fe5bf95c53d21b
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-09-21 07:19:35 -0400

    sremove createDSComponent from tests

--- tests/cases/ref.js
+++ tests/cases/ref.js
@@ -10,36 +10,18 @@
 const DeStagnate = require("../deStagnate.bundle"),
     assert = require("assert")
 
-const {createDSComponent, createElement, createRef} = DeStagnate
-
-class Ref2 extends DeStagnate.default {
-
-    testRef = createRef()
-
-    render = () => createElement("div", {
-        id: "_REFTEST2",
-        ref: this.testRef,
-    })
-
-}
+const {createElement, createRef} = DeStagnate
 
 class Ref extends DeStagnate.default {
 
     testRef = createRef()
-
-    testRef2 = createRef()
 
     render = () => createElement(
         "div",
         {
             ref: this.testRef,
             id: "_REFTEST"
-        },
-        createDSComponent(
-            Ref2,
-            {},
-            this.testRef2
-        )
+        }
     )
 
 }
@@ -55,20 +37,6 @@ module.exports.test = (document) => {
         assert.strictEqual(
             "_REFTEST",
             ref.testRef.current.id,
-        )
-    })
-
-    it("Should have an ID of _REFTEST2", () => {
-        assert.strictEqual(
-            "_REFTEST2",
-            ref.testRef2.current.testRef.current.id,
-        )
-    })
-
-    it("Should be instanceof Ref2", () => {
-        assert.strictEqual(
-            true,
-            ref.testRef2.current instanceof Ref2,
         )
     })
 }
