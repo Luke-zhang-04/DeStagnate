4ebce8ed7b72f81f83e9950ff14e5db0d83878c8
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-02-07 21:22:37 -0500

    update exports
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,25 @@
 // eslint-disable-next-line
 /// <reference path="./jsx.ts" />
 
+import * as _Component from "./component"
+import * as _CreateRef from "./createRef"
+import * as _CreateElement from "./createElement"
+import * as _CreateElementNS from "./createElementNS"
+import * as _Fragment from "./fragment"
+
 export {Component} from "./component"
-
 export {Ref, createRef} from "./createRef"
-
 export {createElement} from "./createElement"
-
 export {createElementNS} from "./createElementNS"
+export {fragment} from "./fragment"
+
+export namespace DeStagnate {
+    export const {Component} = _Component
+    export const {createRef} = _CreateRef
+    export type Ref = _CreateRef.Ref
+    export const {createElement} = _CreateElement
+    export const {createElementNS} = _CreateElementNS
+    export const {fragment} = _Fragment
+}
+
+export default DeStagnate
