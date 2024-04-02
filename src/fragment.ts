8da467585bdb00436adaedd7e7ba6d5eb45e93e6
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-04-02 09:47:46 -0400

    feat: support refs to fragments

--- src/fragment.ts
+++ src/fragment.ts
@@ -2,11 +2,20 @@
 /// <reference path="./jsx.ts" />
 
 import {ChildrenArrayType, bindChildren} from "./internal/_createElementUtils"
+import type {Ref} from "destagnate"
+
+interface FragmentProps {
+    ref: Ref<DocumentFragment>
+}
 
 /* eslint-disable @typescript-eslint/naming-convention */
-export const Fragment = (_props: unknown, ...children: ChildrenArrayType): DocumentFragment => {
+export const Fragment = (
+    props: FragmentProps,
+    ...children: ChildrenArrayType
+): DocumentFragment => {
     const documentFragment = document.createDocumentFragment()
 
+    props.ref.current = documentFragment
     bindChildren(documentFragment, children)
 
     return documentFragment
