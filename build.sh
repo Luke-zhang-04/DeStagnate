5d8b5ff34b24732c5212192b5cfb7309fe9fed71
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-04-03 18:16:02 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Build shell script for DeStagnate
-# Copyright (C) 2020 - 2021 Luke Zhang | MIT License | luke-zhang-04.github.io
-
 source "./colours.sh" # Source colours file
 
 bin="node_modules/.bin"
