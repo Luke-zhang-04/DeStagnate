2e9a8ae7ff1ded7af88bf35181da51a02c39e685
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-04-07 22:20:54 -0400

    test: add another FC test

--- test/cases/functionComponent.js
+++ test/cases/functionComponent.js
@@ -2,14 +2,43 @@ import assert from "assert"
 import {createElement} from "../destagnate.js"
 
 export const test = () => {
-    const myComponent = ({text}) => createElement("p", {id: "FCTest"}, text)
-
-    document.body.appendChild(createElement(myComponent, {text: "Testing function component"}))
+    const myComponent = ({text, id = "FCTest"}) => createElement("p", {id}, text)
+    const myComponent2 = ({text, id = "FCTest2"}, ...children) =>
+        createElement(
+            "p",
+            {id},
+            createElement("p", null, text),
+            createElement("div", null, children),
+        )
 
     it('Should equal "Testing function component"', () => {
+        document.body.appendChild(createElement(myComponent, {text: "Testing function component"}))
+
         assert.strictEqual(
             "Testing function component",
             document.querySelector("#FCTest").innerHTML,
+        )
+    })
+
+    it('Should equal "Testing function component 2"', () => {
+        document.body.appendChild(
+            createElement(
+                myComponent2,
+                {text: "Testing function component 2"},
+                "Child1",
+                0,
+                "child2",
+                createElement("a"),
+            ),
+        )
+
+        assert.strictEqual(
+            "Testing function component 2",
+            document.querySelector("#FCTest2").firstChild.innerHTML,
+        )
+        assert.strictEqual(
+            "Child10child2<a></a>",
+            document.querySelector("#FCTest2").lastChild.innerHTML,
         )
     })
 }
