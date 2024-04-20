8cd1eca81c47b32fbf924051ab6d7562040a1980
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-04-20 16:35:53 -0400

    chore: update version

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ const bannerDev = `/**
  * DeStagnate: A lightweight wrapper around vanilla DOM methods
  *
  * @license MIT
- * @version 3.1.0
+ * @version 3.2.0
  * @copyright 2020 - 2024 Luke Zhang
  */`
 
